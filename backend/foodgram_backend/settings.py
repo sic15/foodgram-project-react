7ca5e2c6e88bc6ae35b99d26ef9800651f08950e
Наталья Арлазарова <sic15@yandex.ru>
2023-10-04 23:14:18 +0300

    33

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -150,4 +150,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://localhost:8000', 'http://62.84.123.59']
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://localhost:8000', 'http://62.84.123.59', 'https://tasty-food.sytes.net']
