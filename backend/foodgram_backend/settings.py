420b85c7c5beaf6cf1feeb5676407af35e097ba7
Наталья Арлазарова <sic15@yandex.ru>
2023-10-06 00:44:01 +0300

    68

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -90,7 +90,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
-
 # для тестирования на базе данных SQLite необходимо раскомментировать строки 96-102
 # и закомментировать строки 104-113
 
