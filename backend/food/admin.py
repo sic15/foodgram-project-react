70d8b0c2724184a8d9af1efff5f3adc205784be0
Наталья Арлазарова <sic15@yandex.ru>
2023-10-12 11:31:32 +0300

    исправлены ошибки +1

--- backend/food/admin.py
+++ backend/food/admin.py
@@ -4,15 +4,8 @@ from .models import AmountIngredient, Favorite, Ingredient, Recipe, Tag
 
 admin.site.register(Ingredient)
 admin.site.register(Favorite)
-"""
-class AmountInline(admin.StackedInline):
-    model = AmountIngredient
-    readonly_fields = ('measurement_unit', )
-    fields = ['recipe', 'ingredient', ('amount', 'measurement_unit')]
 
-    def measurement_unit(self, obj):
-        return obj.ingredient.measurement_unit
-"""
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     readonly_fields = ('sudscriptions',)
@@ -20,7 +13,6 @@ class RecipeAdmin(admin.ModelAdmin):
               'cooking_time', 'image', 'tags', 'sudscriptions']
     list_display = ['name', 'author']
     list_filter = ['author', 'name', 'tags']
- #   inlines = [AmountInline]
 
     def sudscriptions(self, obj):
         return Favorite.objects.filter(recipe=obj).count()
@@ -29,8 +21,6 @@ class RecipeAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     fields = ['name', 'color']
-
-#admin.site.register(AmountIngredient)
 
 
 @admin.register(AmountIngredient)
